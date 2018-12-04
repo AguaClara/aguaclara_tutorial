f99459ab071770bd6de98401172261e7b5a9cf67
Oliver Leung <oal22@cornell.edu>
2018-12-04 14:41:43 -0500

    Change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, AguaClara Cornell'
 author = 'AguaClara Cornell'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.0'
+release = 'v0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -82,7 +82,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
